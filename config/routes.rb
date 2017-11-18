891a485f405a6698e0b21dbb8be400cfc8c66704
haydencarlson <hayden.carlson@icloud.com>
2017-11-17 23:08:33 -0800

    Add bootstrap, jquery and navbar & move devise views to views folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    authenticated  do
+      root to: 'pages#dashboard'
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: 'unauthenticated_root'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
