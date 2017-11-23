7653e5cee9b1997fb07e83fa04cb2ab134f917eb
Hayden Carlson <haydog2@hotmail.com>
2017-11-22 22:49:41 -0800

    Change root authenticated route to routes index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     authenticated :user  do
-      root to: 'earns#index'
+      root to: 'offers#index'
     end
 
     unauthenticated do
