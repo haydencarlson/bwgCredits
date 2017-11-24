aaec73e4a15ce874fe0f5b833171f681404c93a4
Hayden Carlson <haydog2@hotmail.com>
2017-11-24 00:14:58 -0800

    Add sidebar information

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,5 +2,7 @@ class OffersController < ApplicationController
   def index
     @offers = Offer.order(:created_at => :asc).page params[:page]
     @total_completed_offers = Credit.all.count
+    @total_credits_rewarded = Credit.all.where('amount > ?', 0).sum(:amount)
+    @total_credits_redeemed = Credit.all.where('amount < ?', 0).sum(:amount) * -1
   end
 end
