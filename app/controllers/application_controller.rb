743384a2823558039aa9a759721c1d06c3562359
Hayden Carlson <haydog2@hotmail.com>
2017-11-23 22:09:42 -0800

    Render offers on offers pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def get_user_credit_balance
+    current_user.credits.sum(:amount)
+  end
+  helper_method :get_user_credit_balance
 end
