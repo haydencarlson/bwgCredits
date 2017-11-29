41da7fc3e457a9b292a83474fb78be61131e4ea6
Hayden Carlson <haydog2@hotmail.com>
2017-11-28 19:55:49 -0800

    Remove sidebar from offers index. Add to application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :sidebar_data
+  
   def get_user_credit_balance
     current_user.credits.sum(:amount)
   end
   helper_method :get_user_credit_balance
+
+  def sidebar_data
+    @offers = Offer.order(:created_at => :asc).page params[:page]
+    @total_completed_offers = Credit.all.count
+    @total_credits_rewarded = Credit.all.where('amount > ?', 0).sum(:amount)
+    @total_credits_redeemed = Credit.all.where('amount < ?', 0).sum(:amount) * -1
+    @total_users = User.all.count
+    @total_offers = Offer.all.count
+  end
 end
